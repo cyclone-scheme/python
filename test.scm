c65617df077c59fb95d4c605a66e533735eb3223
Arthur Maciel <arthurmaciel@gmail.com>
2021-02-21 13:25:58 -0300

    Fixed test

--- test.scm
+++ test.scm
@@ -3,9 +3,6 @@
         (cyclone test)
         (cyclone python)
         (srfi 69))
-(c-compiler-options "-g")
-
-;; (test-begin)
 
 (py-start)
 (py-import "math")
@@ -23,17 +20,5 @@
   (py-def "math.floor" n)
   (test 3 (math.floor 3.14159265358979)))
 
-;; (test-group "py-define-attribute"
-
-;;   )
-
-;; (define l (py-list-new 2))
-;; (py-list-set-item l 0 '(1 2 3))
-;; (py-list-set-item l 1 '(4 5 6))
-
-;; (define-pyfun "numpy.array")
 (py-stop)
-;; (test-end)
 
-
-(test-exit)
